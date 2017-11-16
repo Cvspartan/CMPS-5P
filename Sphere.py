2a299c9a6f0d8293f378281c36a410d5069369b7
Christopher Trung-Huy Vu <cvu4@unix.lt.ucsc.edu>
2017-11-15 23:04:50 -0800

    First commit

---
+++ Sphere.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Enter the radius of the sphere: "))
+volume = (4/float(3))*math.pi*radius**3
+surface_area = 4*math.pi*radius*radius
+
+print("The volume is: "+str(volume))
+print("The surface area is "+str(surface_area))
